0ad5e60b36ec37dd98586c89fec0d3b8f2777ed7
Mark Leslie Plowright <90641606+Markplowwright@users.noreply.github.com>
2025-05-24 13:38:52 -0400

    Update vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  root: './project',        // <-- add this to set the root folder
-  base: '/Resume/',         // essential for GitHub Pages repo subpath
+  root: 'project',       // << here, points to your subfolder with index.html
+  base: '/Resume/',
   plugins: [react()],
   optimizeDeps: {
     include: ['react', 'react-dom', 'react-globe.gl', 'three', 'd3'],
@@ -13,7 +13,20 @@ export default defineConfig({
     commonjsOptions: {
       include: [/node_modules/],
       transformMixedEsModules: true
-    }
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react')) return 'vendor-react';
+            if (id.includes('three')) return 'vendor-three';
+            if (id.includes('d3')) return 'vendor-d3';
+            return 'vendor';
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
   },
   resolve: {
     alias: {
@@ -26,4 +39,3 @@ export default defineConfig({
     }
   }
 });
-
